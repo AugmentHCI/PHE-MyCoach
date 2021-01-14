6997c2340ad36e71b169209ff897426046e5fd10
Maxwell <maxwell.szymanski@student.kuleuven.be>
2021-01-14 18:38:35 +0100

    Navigation Bar back button

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -11,7 +11,6 @@ class App extends Component {
   render() {
     return(
       <div>
-        <NavigationBar></NavigationBar>
         {this.props.content}
       </div>
     )
--- imports/ui/components/NavigationBar.jsx
+++ imports/ui/components/NavigationBar.jsx
@@ -7,18 +7,11 @@ function NavigationBar(props) {
     
     const T = i18n.createComponent("Common");
 
-    const [isPressed, press] = useState(false);
-
-    function action() {
-        press(false); 
-        console.log("Action");
-        if (props.action) {console.log("Action"); props.action()};
-    }
-
     if (noNavigationBar.includes(FlowRouter.current().route.name)) return (<React.Fragment/>);
 
     return (
-        <div className="NavigationBar">
+        <div className="navigation-bar">
+            <button className="navigation-bar-button" onClick={() => history.back()}>{'<'}</button>
             {FlowRouter.current().route.name}
         </div>
     )
