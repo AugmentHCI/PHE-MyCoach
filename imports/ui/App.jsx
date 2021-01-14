496aa20e4808258e7feafc4932f7db67058d0c0b
Maxwell <maxwell.szymanski@student.kuleuven.be>
2021-01-14 18:32:51 +0100

    NavBar Update

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import NavigationBar from '../ui/components/NavigationBar.jsx';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../client/main.css';
 
@@ -9,6 +11,7 @@ class App extends Component {
   render() {
     return(
       <div>
+        <NavigationBar></NavigationBar>
         {this.props.content}
       </div>
     )
