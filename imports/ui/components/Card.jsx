8d3f6a2f8abf57d7b75f1eda751069627a27a13e
Maxwell <maxwell.szymanski@student.kuleuven.be>
2021-01-20 14:27:28 +0100

    Card.jsx - Underline option added

--- imports/ui/components/Card.jsx
+++ imports/ui/components/Card.jsx
@@ -5,9 +5,11 @@ function Card(props) {
     
     const T = i18n.createComponent("Common");
 
+    const titleClass = props.underline ? "card-header-title-underline" : "card-header-title";
+
     return (
         <div className="card">
-            <p className="card-header-title">{props.demo ? props.title : <T>{props.title}</T>}</p>
+            <p className={titleClass}>{props.noTranslate ? props.title : <T>{props.title}</T>}</p>
             <div className="card-body" style={props.bodyStyle}>
                 {props.children}
             </div>
