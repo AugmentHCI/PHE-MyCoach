20ba4490cc24c1710d7c0ebee737750a96f6aa66
Maxwell <maxwell.szymanski@student.kuleuven.be>
2021-01-20 21:46:24 +0100

    Pain-Module part 2

--- imports/ui/Pages/Modules/Content.jsx
+++ imports/ui/Pages/Modules/Content.jsx
@@ -5,11 +5,12 @@ import './Content.scss';
 function Content(props) {
 
     /* Constructors for content */
-    createTextContent = (key, text) => {
-        return <div key={key} className="content-text">{text}</div>;
+
+    function createTextContent(key, text, isOverview) {
+        return <div key={key} className={isOverview ? "content-text-overview" : "content-text"}>{text}</div>;
     }
 
-    createQuestionContent = (key, question, options, correct, onCorrect, onIncorrect) => {
+    function createQuestionContent(key, question, options, correct, onCorrect, onIncorrect) {
         let buttonsHTML = [];
         let [status, changeStatus] =  useState("default");
         /* Make buttons for each of the options, and assign correct/incorrect actions to buttons */
@@ -33,7 +34,22 @@ function Content(props) {
         </div>);
     };
 
-    createVideoContent = (key, link, language) => {
+    function createListContent(key, content, isNumbered, isOverview) {
+        let listHTML = [];
+        content.forEach((item, index) => {
+            listHTML.push(<li key={key + "-" + index} className={isOverview ? "content-list-item-overview" : "content-list-item"}>{item}</li>)
+        });
+        if (isNumbered) return <ol className="content-list">{listHTML}</ol>;
+        else return <ul className="content-list">{listHTML}</ul>;
+    }
+
+    /**
+     * 
+     * @param {String} key The key of the HTML element
+     * @param {Dict} link A dictionary of language-keys and corresponding link-values
+     * @param {String} language The language used as the key for the link dictionary
+     */
+    function createVideoContent(key, link, language) {
         return <iframe key={key} src={link[language]} width="100%" style={{marginTop:"20px", borderRadius: "10px"}} frameBorder="0" allow="autoplay; fullscreen"></iframe>
     }
 
@@ -47,7 +63,9 @@ function Content(props) {
     /* Generate content according to content type */
     switch (props.data.type) {
         case 'Text':
-            return createTextContent(props.data.key, props.data.content);
+            return createTextContent(props.data.key, props.data.content, props.data.overview);
+        case 'List':
+            return createListContent(props.data.key, props.data.content, props.data.numbered, props.data.overview);
         case 'Question':
             return createQuestionContent(props.data.key, props.data.question, props.data.options, props.data.correct, props.data.onCorrect, props.data.onIncorrect);
         case 'Video':
--- imports/ui/components/Card.jsx
+++ imports/ui/components/Card.jsx
@@ -7,11 +7,12 @@ function Card(props) {
 
     const titleClass = props.underline ? "card-header-title-underline" : "card-header-title";
     const cardClass = props.overview ? "card-overview" : "card";
+    const cardBodyClass = props.overview ? "card-body-overview" : "card-body";
 
     return (
         <div className={cardClass}>
             <p className={titleClass}>{props.noTranslate ? props.title : <T>{props.title}</T>}</p>
-            <div className="card-body" style={props.bodyStyle}>
+            <div className={cardBodyClass} style={props.bodyStyle}>
                 {props.children}
             </div>
         </div>
