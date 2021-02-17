9068bbc90d860ded36420e70c49bc282edc57e62
Maxwell <maxwell.szymanski@student.kuleuven.be>
2021-02-17 08:08:21 +0100

    Fixed Button Selection

--- imports/ui/components/Button.jsx
+++ imports/ui/components/Button.jsx
@@ -14,12 +14,15 @@ export default function Button(props) {
     let buttonClass = props.disabled ? "button-inactive" : "button";
     buttonClass += isPressed ? " pressed" : "";
     if (props.isSelected) buttonClass += " selected";
+    console.log(props.isSelected);
 
     /* Generate Button Style */
     let style = props.style ? props.style : {};
-    style["backgroundColor"] = props.color ? `var(--idewe-${props.color})` : "var(--idewe-white)"
-    style["color"] = props.contentColor ? `var(--idewe-${props.color})` : (props.color ? "var(--idewe-white)" : "var(--idewe-blue)")
-    if (props.width === "fit" ) style["width"] = "fit-content";
+    if (!props.isSelected) {
+        style["backgroundColor"] = props.color ? `var(--idewe-${props.color})` : "var(--idewe-white)"
+        style["color"] = props.contentColor ? `var(--idewe-${props.color})` : (props.color ? "var(--idewe-white)" : "var(--idewe-blue)")
+        if (props.width === "fit" ) style["width"] = "fit-content";
+    }
 
     return (
         <button className={buttonClass} 
