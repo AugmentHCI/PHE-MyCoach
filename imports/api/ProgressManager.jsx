c4e0fa4aa52a015adc89e636a8949c8b6656b0a5
Maxwell <maxwell.szymanski@student.kuleuven.be>
2021-04-04 21:34:19 +0200

    ProgressManager - Unlock Flow

--- imports/api/ProgressManager.jsx
+++ imports/api/ProgressManager.jsx
@@ -1,8 +1,9 @@
 import "../db/MyCoachMethods.jsx";
 
 export default class ProgressManager {
-    constructor(userID) {
+    constructor(userID, profile=3) {
         this.userID = userID;
+        this.profile = profile;
     }
 
     /**
@@ -11,27 +12,8 @@ export default class ProgressManager {
      * @returns A dictionary with the modules, and submodule progress
      */
     async getModuleProgress(module) {
-        let progress = JSON.parse(JSON.stringify(template));
-        const result = await Meteor.callPromise('mycoachprogress.getModuleProgress', {userID: this.userID, moduleID: module});
-        result.forEach(data => {
-            /* Only reassign with higher priorities (e.g. do not override "COMPLETED" with "IN_PROGRESS" */
-            switch (progress[data.moduleID][data.submoduleID]) {
-                case undefined:
-                    progress[data.moduleID][data.submoduleID] = data.status;
-                    break;
-                case "NOT_STARTED":
-                    if (["IN_PROGRESS", "COMPLETED"].includes(data.status)) progress[data.moduleID][data.submoduleID] = data.status;
-                    break;
-                case "IN_PROGRESS":
-                    if (data.status === "COMPLETED") progress[data.moduleID][data.submoduleID] = data.status;
-                    break;
-                case "COMPLETED":
-                    break;
-                default:
-                    break;
-            }
-        });
-        return progress;
+        const moduleUserData = await Meteor.callPromise('mycoachprogress.getModuleProgress', {userID: this.userID, moduleID: module});
+        return parseProgress(moduleUserData);
     }
 
     /**
@@ -39,18 +21,120 @@ export default class ProgressManager {
      * @param {String}  module    The module that the submodule belongs to.
      * @param {String}  submodule The submodule that is completed.
      * @param {String}  status    The new status of the given submodule (e.g. "COMPLETED").
-     * @param {Boolean} isLast    Boolean indicating if this is the last submodule of the module or not (to unlock new modules).
      */
-    finishSubmodule(module, submodule, status, isLast) {
-        Meteor.call('mycoachprogress.setProgress', {userID: this.userID, moduleID: module, submoduleID: submodule, status: status});
-        if (!isLast) {
+    async finishSubmodule(module, submodule, status) {
+        await Meteor.callPromise('mycoachprogress.setProgress', {userID: this.userID, moduleID: module, submoduleID: submodule, status: status});
+        const allUserData = await Meteor.callPromise('mycoachprogress.getUserProgress', {userID: this.userID});
+        const progress = parseProgress(allUserData);
+        if (getModuleStatus(progress[module]) !== "COMPLETED") {
+            console.log("Unlocking submodule");
+            /* Unlock subsequent submodule within same module */
             const nextSubmodule = submodule.split("_")[0] + "_" + submodule.split("_")[1] + "_" + String(parseInt(submodule.split("_")[2]) + 1);
             Meteor.call('mycoachprogress.setProgress', {userID: this.userID, moduleID: module, submoduleID: nextSubmodule, status: "IN_PROGRESS"});
-        }  
+        }
         else {
-            /* Implement Module unlocking mechanism */
+            /* Unlock next module(s), depending on status and profile */
+            console.log("Unlocking next modules");
+            switch (this.profile) {
+                case 1:
+                case 4:
+                    if (getModuleStatus(progress["MOVEMENT"]) === "NOT_STARTED") this.unlockModules(["MOVEMENT"], progress);
+                    else if (getModuleStatus("MOVEMENT") === "COMPLETED") this.unlockModules(["THOUGHTSEMOTIONS", "ACTIVITYWORK", "STRESS", "SOCIAL"], progress);
+                    break;
+                case 2:
+                    if (getModuleStatus(progress["MOVEMENT"]) === "NOT_STARTED") this.unlockModules(["MOVEMENT"], progress);
+                    else if (getModuleStatus(progress["MOVEMENT"]) === "COMPLETED" && getModuleStatus(progress["THOUGHTSEMOTIONS"]) === "NOT_STARTED") 
+                        this.unlockModules(["THOUGHTSEMOTIONS"], progress);
+                    else if (getModuleStatus(progress["THOUGHTSEMOTIONS"]) === "COMPLETED") this.unlockModules(["ACTIVITYWORK", "STRESS", "SOCIAL"], progress);
+                    break; 
+                case 3:
+                    if (getModuleStatus(progress["THOUGHTSEMOTIONS"]) === "NOT_STARTED") 
+                        { this.unlockModules(["THOUGHTSEMOTIONS"], progress); }
+                    else if (getModuleStatus(progress["THOUGHTSEMOTIONS"]) === "COMPLETED" && (getModuleStatus(progress["ACTIVITYWORK"]) === "NOT_STARTED" || getModuleStatus(progress["STRESS"]) === "NOT_STARTED" || getModuleStatus(progress["SOCIAL"]) === "NOT_STARTED")) 
+                        { this.unlockModules(["ACTIVITYWORK", "STRESS", "SOCIAL"], progress); }
+                    else if (getModuleStatus(progress["ACTIVITYWORK"]) === "COMPLETED" && getModuleStatus(progress["STRESS"]) === "COMPLETED" && getModuleStatus(progress["SOCIAL"]) === "COMPLETED") 
+                        { this.unlockModules(["MOVEMENT"], progress); }
+                    break; 
+                case 5:
+                case 6:
+                    if (getModuleStatus(progress["ACTIVITYWORK"]) === "NOT_STARTED" || getModuleStatus(progress["THOUGHTSEMOTIONS"]) === "NOT_STARTED") 
+                        { this.unlockModules(["ACTIVITYWORK", "THOUGHTSEMOTIONS"], progress); }
+                    else if (getModuleStatus(progress["ACTIVITYWORK"]) === "COMPLETED" && getModuleStatus(progress["THOUGHTSEMOTIONS"]) === "COMPLETED" && (getModuleStatus(progress["STRESS"]) === "NOT_STARTED" || getModuleStatus(progress["SOCIAL"]) === "NOT_STARTED")) 
+                        { this.unlockModules(["STRESS", "SOCIAL"], progress); }
+                    else if (getModuleStatus(progress["STRESS"]) === "COMPLETED" && getModuleStatus(progress["SOCIAL"]) === "COMPLETED") 
+                        { this.unlockModules(["MOVEMENT"], progress); }
+                    break;
+                default:
+                    console.log("No profile provided for user.")
+                    break;
+            }
         }
     }
+
+    /**
+     * Unlocks the given array of modules for the user (and does nothing for already unlocked modules).
+     * @param {[String]} modules The array of module-codes that need to be unlocked
+     */
+    unlockModules(modules, progress) {
+        modules.forEach(module => {
+            if (getModuleStatus(progress[module]) === "NOT_STARTED") {
+                switch (module) {
+                    case "PAINEDUCATION": 
+                        Meteor.call('mycoachprogress.setProgress', {userID: this.userID, moduleID: "PAINEDUCATION", submoduleID: "PE_MOD_1", status: "IN_PROGRESS"}); 
+                        break;
+                    case "THOUGHTSEMOTIONS": 
+                        Meteor.call('mycoachprogress.setProgress', {userID: this.userID, moduleID: "THOUGHTSEMOTIONS", submoduleID: "TE_MOD_1", status: "IN_PROGRESS"}); 
+                        break;
+                    case "ACTIVITYWORK": 
+                        Meteor.call('mycoachprogress.setProgress', {userID: this.userID, moduleID: "ACTIVITYWORK", submoduleID: "ACT_MOD_1", status: "IN_PROGRESS"}); 
+                        break;
+                    case "MOVEMENT": 
+                        Meteor.call('mycoachprogress.setProgress', {userID: this.userID, moduleID: "MOVEMENT", submoduleID: "MOV_MOD_1", status: "IN_PROGRESS"}); 
+                        break;
+                    case "STRESS": 
+                        Meteor.call('mycoachprogress.setProgress', {userID: this.userID, moduleID: "STRESS", submoduleID: "STR_MOD_1", status: "IN_PROGRESS"}); 
+                        break;
+                    case "SOCIAL": 
+                        Meteor.call('mycoachprogress.setProgress', {userID: this.userID, moduleID: "SOCIAL", submoduleID: "SOC_MOD_1", status: "IN_PROGRESS"}); 
+                        break;
+                }
+            }
+        });
+    }
+}
+
+function getModuleStatus(submodules) {
+    let completed = true, notStarted = true;
+    for (const [submodule, status] of Object.entries(submodules)) {
+        if (status !== "COMPLETED")   completed  = false;
+        if (status !== "NOT_STARTED") notStarted = false;
+    }
+    if (completed) return "COMPLETED";
+    if (notStarted) return "NOT_STARTED";
+    return "IN_PROGRESS"
+}
+
+function parseProgress(userData) {
+    let progress = JSON.parse(JSON.stringify(template));
+    userData.forEach(data => {
+        /* Only (re)assign with higher priorities (e.g. do not override "COMPLETED" with "IN_PROGRESS" */
+        switch (progress[data.moduleID][data.submoduleID]) {
+            case undefined:
+                progress[data.moduleID][data.submoduleID] = data.status;
+                break;
+            case "NOT_STARTED":
+                if (["IN_PROGRESS", "COMPLETED"].includes(data.status)) progress[data.moduleID][data.submoduleID] = data.status;
+                break;
+            case "IN_PROGRESS":
+                if (data.status === "COMPLETED") progress[data.moduleID][data.submoduleID] = data.status;
+                break;
+            case "COMPLETED":
+                break;
+            default:
+                break;
+        }
+    });
+    return progress;
 }
 
 const template = {
@@ -60,5 +144,4 @@ const template = {
         "MOVEMENT": {},
         "STRESS": {},
         "SOCIAL": {}
-
 }
